3e6e43d7722825467e9fff6a2f0b9dc19ed7a60a
Onishi1129 <taku.hot.2545@gmail.com>
2021-07-25 14:29:34 +0000

    追加

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '27672df7ab4149e1287176c2343d756ee18916d74ac1cf9445788f6acfca84d5221169bb6cfca59cb77e369e079e976f02df3a18715d48a932d76d793f645655'
